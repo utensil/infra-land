7a33f3850ff1bcd779ee98fb2d48dcf1ba3ecdd5
Peri <utensilcandel@gmail.com>
2026-08-03 18:09:03 +0800

    feat: add Forest Helix workspace [AGENT]
    Add the documented Forester LSP and pinned tree-sitter highlighting while leaving the Lean-focused Helix fork and unrelated editor settings intact. Add idempotent Herdr Browser setup and a three-pane Forest workspace with hx-lean, the live Forest server, and a Chromium preview.

---
+++ dotfiles/.config/helix/runtime/queries/forester/highlights.scm
@@ -0,0 +1,60 @@
+(comment) @comment
+
+[
+ "\\"
+ "("
+ ")"
+ "{"
+ "}"
+ "["
+ "]"
+] @punctuation.bracket
+
+(p "p" @function.builtin)
+(li "li" @markup.list)
+(ul "ul"  @markup.list)
+(ol "ol"  @markup.list)
+(em "em"  @function.builtin)
+(strong "strong" @function.builtin)
+(code "code" @function.builtin)
+
+(tag "tag" @field)
+(author "author" @field)
+(contributor "contributor" @field)
+(title "title" @field)
+(taxon "taxon" @field)
+(meta "meta" @field)
+
+(title "title" @text.title)
+(title (_) @text.title)
+(author author: (_) @markup.heading.url)
+
+(ident label: (_) @string)
+(transclude "transclude" @include)
+(transclude address: (_) @markup.link.url)
+
+(def "def" @keyword)
+(object "object" @constant)
+(object self: (_) @keyword)
+(method_decl key: (_) @method)
+(patch "patch" @text.diff.add)
+(patch object: (_) @constant)
+
+(markdown_link label: (_) @label)
+(markdown_link dest: (_) @text.uri)
+(unlabeled_link (external_link (_) @text.uri))
+
+(scope "scope" @namespace)
+(put "put" @variable.parameter)
+
+(query_tree "query" @keyword)
+;(query_author "query/author" @keyword)
+;(query_tag "query/tag" @keyword)
+;(query_taxon "query/taxon" @keyword)
+;(query_and "query/and" @keyword)
+;(query_or "query/or" @keyword)
+;(query_meta "query/meta" @keyword)
+
+(import "import" @include)
+(export "export" @include)
+(transclude "transclude" @include)
